8df978f73d8859593df375abf526d78a7713824b
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-02 14:59:33 +0200

    Fas 1 Steg: Registrera SQLite DbContext i Program.cs. Closes #18

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MovieApi_Refactor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<MovieContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("Data"))
+        );
 
 var app = builder.Build();
 
