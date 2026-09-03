49ce0778c9d1f4ffc41db97b866dba603ad08761
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-03 09:30:55 +0200

    Fas 3 Steg 1: Skapa IMovieRepository. Closes #28

---
+++ Repositories/IMovieRepository.cs
@@ -0,0 +1,14 @@
+using MovieApi_Refactor.Entities;
+
+namespace MovieApi_Refactor.Repositories;
+
+public interface IMovieRepository
+{
+    Task<IEnumerable<Movie>> GetAllAsync();
+    Task<Movie?> GetById(int id);
+    void Add(Movie movie);
+    void Update(Movie movie);
+    void Remove(Movie movie);
+
+    Task<bool> SaveChagesAsync();
+}
