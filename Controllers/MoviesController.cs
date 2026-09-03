14064623f7063f31b0f28cdf4a9b3fc509dcd4a4
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-03 11:45:21 +0200

    Fas 4 Steg 5: Refaktorera MoviesController. Closes #38

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,15 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MovieApi_Refactor.Repositories;
 using MovieApi_Refactor.Entities;
+using MovieApi_Refactor.Data;
 
 namespace MovieApi_Refactor.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class MoviesController(IMovieRepository repository) : ControllerBase
+public class MoviesController(IUnitOfWork unitOfWork) : ControllerBase
 {
     [HttpGet(Name = "Movies")]
-    public async Task<IEnumerable<Movie>> GetAllAsync() => await repository.GetAllAsync();
+    public async Task<IEnumerable<Movie>> CompleteAsync() => await unitOfWork.Movies.GetAllAsync();
 
 }
