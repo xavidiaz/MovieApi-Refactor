c42d55eb58ebefb9f6c82e818a50a56f785e5188
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-08 11:06:43 +0200

    Fas 5 Steg 4: Injicera IMovieService i MovieController. Closes #44

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MovieApi_Refactor.Entities;
-using MovieApi_Refactor.Data;
+using MovieApi_Refactor.Services;
 
 namespace MovieApi_Refactor.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class MoviesController(IUnitOfWork unitOfWork) : ControllerBase
+public class MoviesController(IMovieService movieService) : ControllerBase
 {
     [HttpGet(Name = "Movies")]
-    public async Task<IEnumerable<Movie>> CompleteAsync() => await unitOfWork.Movies.GetAllAsync();
+    public async Task<IEnumerable<Movie>> GetAllAsync() => await movieService.GetAllAsync();
 
 }
