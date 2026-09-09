c13361fde3959a03b2492058e1a375931dce0580
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-09 11:21:19 +0200

    Fas 6 Steg 8: Implementera ActorService. Closes #54

---
+++ Services/ActorService.cs
@@ -0,0 +1,45 @@
+using MovieApi_Refactor.Data;
+using MovieApi_Refactor.Entities;
+
+namespace MovieApi_Refactor.Services;
+
+public class ActorService(IUnitOfWork unitOfWork) : IActorService
+{
+    public async Task<IEnumerable<Actor>> GetAllAsync() => await unitOfWork.Actors.GetAllAsync();
+
+    public async Task<Actor?> GetByIdAsync(int id) => await unitOfWork.Actors.GetByIdAsync(id);
+
+    public async Task<Actor> CreateAsync(Actor actor)
+    {
+        unitOfWork.Actors.Add(actor);
+        await unitOfWork.CompleteAsync();
+        return actor;
+    }
+
+    public async Task<Actor?> UpdateAsync(int id, Actor actor)
+    {
+        var existing = await unitOfWork.Actors.GetByIdAsync(id);
+        if (existing is null)
+            return null;
+
+        existing.FirstName = actor.FirstName;
+        existing.LastName = actor.LastName;
+        existing.BirthYear = actor.BirthYear;
+
+        existing.Movies = actor.Movies;
+
+        await unitOfWork.CompleteAsync();
+        return existing;
+    }
+
+    public async Task<Actor?> DeleteAsync(int id)
+    {
+        var actor = await unitOfWork.Actors.GetByIdAsync(id);
+        if (actor is null)
+            return null;
+
+        unitOfWork.Actors.Remove(actor);
+        await unitOfWork.CompleteAsync();
+        return actor;
+    }
+}
