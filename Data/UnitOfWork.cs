fa322d326d92bea223bac5dda1336555f101fea0
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-09 15:20:29 +0200

    Fas 6 Steg 17: Lägg till Review i IUnitOfWork + UnitOfWork. Closes #63

--- Data/UnitOfWork.cs
+++ Data/UnitOfWork.cs
@@ -6,6 +6,7 @@ public class UnitOfWork(MovieContext context) : IUnitOfWork
 {
     public IMovieRepository Movies { get; } = new MovieRepository(context);
     public IActorRepository Actors { get; } = new ActorRepository(context);
+    public IReviewRepository Reviews { get; } = new ReviewRepository(context);
 
     public async Task<bool> CompleteAsync() => await context.SaveChangesAsync() > 0;
 }
