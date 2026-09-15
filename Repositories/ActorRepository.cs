dcddb725b2cb68434d9acf0ca8ba2f6eedaa136e
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-15 15:49:14 +0200

    Fas 8: Lägg till Include för Movies i ActorRepository.GetByIdAsync

--- Repositories/ActorRepository.cs
+++ Repositories/ActorRepository.cs
@@ -11,7 +11,8 @@ public class ActorRepository(MovieContext context) : IActorRepository
     public async Task<IEnumerable<Actor>> GetByIdsAsync(IEnumerable<int> ids) =>
         await context.Actors.Where(a => ids.Contains(a.Id)).ToListAsync();
 
-    public async Task<Actor?> GetByIdAsync(int id) => await context.Actors.FindAsync(id);
+    public async Task<Actor?> GetByIdAsync(int id) =>
+        await context.Actors.Include(m => m.Movies).FirstOrDefaultAsync(a => a.Id == id);
 
     public void Add(Actor actor) => context.Actors.Add(actor);
 
