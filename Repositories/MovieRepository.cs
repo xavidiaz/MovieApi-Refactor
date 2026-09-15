dcc9ed7b2d26e9c0e0cdaf987090ddcbb89d10dd
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-15 10:59:33 +0200

    Fas 8: Lägg till Include för Actors & Reviews i MovieRepository.GetByIdAsync

--- Repositories/MovieRepository.cs
+++ Repositories/MovieRepository.cs
@@ -7,9 +7,16 @@ namespace MovieApi_Refactor.Repositories;
 public class MovieRepository(MovieContext context) : IMovieRepository
 {
     public async Task<IEnumerable<Movie>> GetAllAsync() => await context.Movies.ToListAsync();
-    public async Task<Movie?> GetByIdAsync(int id) => await context.Movies.FindAsync(id);
-    public void Add(Movie movie) => context.Movies.Add(movie);
-    public void Update(Movie movie) => context.Movies.Update(movie);
-    public void Remove(Movie movie) => context.Movies.Remove(movie);
 
+    public async Task<Movie?> GetByIdAsync(int id) =>
+        await context
+            .Movies.Include(a => a.Actors)
+            .Include(r => r.Reviews)
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+    public void Add(Movie movie) => context.Movies.Add(movie);
+
+    public void Update(Movie movie) => context.Movies.Update(movie);
+
+    public void Remove(Movie movie) => context.Movies.Remove(movie);
 }
