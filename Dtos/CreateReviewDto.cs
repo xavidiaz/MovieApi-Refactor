950912179ef7c8f408557e42f97f8ba6bf6d1ce8
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-17 13:12:31 +0200

    Fas 10 Steg 3: Lägg till Data Annotations på ReviewDtos. Closes #91

--- Dtos/CreateReviewDto.cs
+++ Dtos/CreateReviewDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieApi_Refactor.Dtos;
 
 public class CreateReviewDto
 {
+    [Range(1.0, 5.0, ErrorMessage = "Rating must be between 1.0 and 5.0.")]
     public required double Rating { get; init; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Review text cannot be empty.")]
+    [StringLength(
+        1000,
+        MinimumLength = 5,
+        ErrorMessage = "Review text must be between 5 and 1000 characters."
+    )]
     public required string Text { get; init; }
 
+    [Range(1, int.MaxValue, ErrorMessage = "A valid MovieId is required.")]
     public required int MovieId { get; init; }
 }
