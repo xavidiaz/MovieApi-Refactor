41329fd0fa20a03c2872c5a2bb653483941e861a
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-17 15:15:31 +0200

    Fas 10 Steg 8: Skapa global exception handler. Closes #96

---
+++ Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi_Refactor.Exceptions;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        if (exception is NotFoundException)
+            return false;
+
+        httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+
+        await httpContext.Response.WriteAsJsonAsync(
+            new ProblemDetails
+            {
+                Status = StatusCodes.Status404NotFound,
+                Title = "Resursen hitades inte",
+                Detail = exception.Message,
+            },
+            cancellationToken
+        );
+        return true;
+    }
+}
