a1b04b9213a2d5e7772ffd63bc51660379817dc1
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-17 13:12:17 +0200

    Fas 10 Steg 1: Lägg till Data Annotations på MovieDtos. Closes #89

--- Dtos/UpdateMovieDto.cs
+++ Dtos/UpdateMovieDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieApi_Refactor.Dtos;
 
 public class UpdateMovieDto
 {
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Title cannot be empty.")]
+    [StringLength(
+        200,
+        MinimumLength = 1,
+        ErrorMessage = "Title must be between 1 and 200 characters."
+    )]
     public required string Title { get; set; }
+
+    [Range(1888, 2100, ErrorMessage = "Please enter a valid release year.")]
     public required int Year { get; set; }
 
+    [MaxLength(100, ErrorMessage = "Cannot assign more than 100 actors to a movie.")]
     public required ICollection<int> ActorsId { get; set; }
 }
